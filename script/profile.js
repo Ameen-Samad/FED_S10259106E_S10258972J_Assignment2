3d95c0180488087ce53edf2a2142d1b31fcfa930
lhs06 <S10258972@connect.np.edu.sg>
2025-02-09 14:55:48 +0800

    fixed code

--- script/profile.js
+++ script/profile.js
@@ -118,3 +118,29 @@ function initializeDropdown() {
         dropdown.addEventListener('mouseleave', () => dropdownContent.classList.remove('show'));
     }
 }
+
+
+/* navbar menu */
+window.onload = function() {
+  const menuToggle = document.getElementById("toggleMenu")
+  const navbar = document.getElementById("nav");
+
+  function toggleMenu() {
+      if (navbar.style.display === 'flex'){
+          navbar.style.display = 'none';
+          menuToggle.textContent = "☰ Menu";
+      } else {
+          navbar.style.display = 'flex';
+          menuToggle.textContent = "✖ Close"; 
+      }
+  }
+
+  window.addEventListener("resize", () => {
+      if (window.innerWidth >= 650) {
+          nav.style.display = "flex";
+      } else {
+          nav.style.display = "none";
+      }
+  });
+  menuToggle.addEventListener('click', toggleMenu)
+}
